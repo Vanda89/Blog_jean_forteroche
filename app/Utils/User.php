f43e6b123dde5acaea35c94c42cdcd25f99ae0c2
Vanda89 <sandrinealcazar@gmail.com>
2018-06-28 18:42:14 +0200

    Création du fichier User.php qui comprend les méthodes de connexion/déconnexion  grâce aux $_SESSION, et détermination du rôle du visiteur du blog.

---
+++ app/Utils/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace P4blog\Utils;
+
+use P4blog\Models\UserModel;
+
+abstract class User
+{
+    public static function isConnected(): bool
+    {
+        return
+            // Si l'utilisateur est connecté
+    array_key_exists('connected-user', $_SESSION) &&
+            // Si l'utilisateur en session est un objet
+    is_object($_SESSION['connected-user']);
+    }
+
+    public static function getConnectedUser()
+    {
+        return $_SESSION['connected-user'];
+    }
+
+    public static function connect(UserModel $userModel): bool
+    {
+        // Ajoute le UserModel du user connecté en SESSION
+        $_SESSION['connected-user'] = $userModel;
+
+        return true;
+    }
+
+    public static function disconnect()
+    {
+        // Je vérifié déjà qu'il y ait un user connecté
+        if (self::isConnected()) {
+            // On supprime le user en SESSION
+            unset($_SESSION['connected-user']);
+        }
+    }
+
+    public static function isAdmin(): bool
+    {
+        if (array_key_exists('connected-user', $_SESSION) && is_object($_SESSION['connected-user'])) {
+            return (self::getConnectedUser()->getIsAdmin() == 1) ? true : false;
+        } else {
+            return false;
+        }
+    }
+}
